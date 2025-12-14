ac3232a6fd7b8dac0048950da00fea6a1f12f961
EMAA06291 <emyyy212@gmail.com>
2025-12-14 08:11:03 +0200

    Revert "Add project files"
    This reverts commit 5f5cb729ef19031f264d813e8a0de213de5692c3.

--- server/src/routes/statsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const statsController = require('../controllers/statsController');
-
-// GET /api/stats/home - Get home page statistics
-router.get('/home', statsController.getHomeStats);
-
-module.exports = router;
